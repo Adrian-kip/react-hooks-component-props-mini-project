eb8710c9f16c8167bf8649c597cac96d731d63ba
Adrian Kiptoo <cherukip.adrian@gmail.com>
2025-04-22 10:33:19 +0300

    last commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 function App() {
+  const blogData = {
+    name: "Underreacted",
+    image: "data:image/png;base64,...",
+    about: "A blog about learning React",
+    posts: [
+      {
+        id: 1,
+        title: "Components 101",
+        date: "December 15, 2020",
+        preview: "Setting up the building blocks of your site",
+        minutes: 5
+      },
+    ]
+  };
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
